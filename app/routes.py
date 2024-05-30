afd4941bdcf0e33fd70ea734b045725ee1f5f2ae
Emmanuel <ekasomo@corp.free.fr>
2024-05-30 15:39:15 +0200

    try to fix pool connection

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from flask import request, jsonify
 import psycopg2
 from psycopg2 import pool
 from psycopg2.extras import RealDictCursor
+from joblib import load
  
 DB_HOST = 'postgresql-hackaton-jo-11.alwaysdata.net'
 DB_NAME = 'hackaton-jo-11_db_propre'
@@ -10,30 +11,55 @@ DB_USER = 'hackaton-jo-11_api'
 DB_PASS = 'Nwk8!G!NgXfGdk3'
  
 db_pool = psycopg2.pool.ThreadedConnectionPool(1, 20, user=DB_USER, password=DB_PASS, host=DB_HOST, database=DB_NAME)
+
+best_rf_model = load('app/models/best_rf_model_gold_2024.joblib')
+# best_rf_model_silver = load('./models/best_rf_model_silver_2024.joblib')
+# best_rf_model_bronze = load('./models/best_rf_model_bronze_2024.joblib')
+
+def get_db_connection():
+    conn = db_pool.getconn()
+    if conn:
+        return conn
+    else:
+        raise Exception("Connection pool exhausted")
+
+def release_db_connection(conn):
+    if conn:
+        db_pool.putconn(conn)
+
+def with_db_connection(f):
+    def decorated_function(*args, **kwargs):
+        conn = get_db_connection()
+        try:
+            return f(conn, *args, **kwargs)
+        finally:
+            release_db_connection(conn)
+    decorated_function.__name__ = f.__name__
+    return decorated_function
  
 @app.route('/', methods=['GET'])
 def home():
     return "Hackathon Groupe 11 - API !"
 
 @app.route('/countries', methods=['GET'])
-def get_countries():
+@with_db_connection
+def get_countries(conn):
     query = '''
         SELECT DISTINCT country_name
         FROM result_summer
         ORDER BY country_name;
     '''
     
-    conn = db_pool.getconn()
     cur = conn.cursor(cursor_factory=RealDictCursor)
     cur.execute(query)
     countries = [row['country_name'] for row in cur.fetchall()]
     cur.close()
-    conn.close()
     
     return jsonify(countries)
 
 @app.route('/hosts', methods=['GET'])
-def get_hosts():
+@with_db_connection
+def get_hosts(conn):
     query = '''
         SELECT * 
         FROM hosts
@@ -45,12 +71,12 @@ def get_hosts():
     cur.execute(query)
     hosts = cur.fetchall()
     cur.close()
-    conn.close()
 
     return jsonify(hosts)
 
 @app.route('/years', methods=['GET'])
-def get_years():
+@with_db_connection
+def get_years(conn):
     query = '''
         SELECT DISTINCT game_year
         FROM result_summer
@@ -62,29 +88,55 @@ def get_years():
     cur.execute(query)
     years = [row['game_year'] for row in cur.fetchall()]
     cur.close()
-    conn.close()
 
     return jsonify(years)
 
+@app.route('/predict', methods=['GET'])
+@with_db_connection
+def predict(conn):
+    country = request.args.get('country_name')
+
+    if not country:
+        return jsonify({ 'error' : 'Missing required query parameter: country_name' }), 400
+
+    query = '''
+        SELECT *
+        FROM result_summer
+        WHERE country_name = %s
+    '''
+    params = (country,)
+
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    cur.execute(query, params)
+    result = cur.fetchall()
+
+    cur.close()
+
+    return jsonify(result)
+
+    # prediction = best_rf_model.predict(result)
+
+    # print(prediction)
+
 @app.route('/athletes', methods=['GET'])
-def get_athletes():
+@with_db_connection
+def get_athletes(conn):
     query = '''
         SELECT * 
         FROM athletes
         ORDER BY total_medals DESC;
     '''
 
-    conn = db_pool.getconn()
     cur = conn.cursor(cursor_factory=RealDictCursor)
     cur.execute(query)
     athletes = cur.fetchall()
     cur.close()
-    conn.close()
 
     return jsonify(athletes)
  
 @app.route('/medals', methods=['GET'])
-def get_medals():
+@with_db_connection
+def get_medals(conn):
     year = request.args.get('year')
 
     if year:
@@ -120,7 +172,6 @@ def get_medals():
     cur.execute(query, params)
     medals = cur.fetchall()
     cur.close()
-    conn.close()
 
     return jsonify(medals)
  
