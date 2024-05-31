86fefcb8533e41e9bc92f61646234431c33f95eb
Emmanuel <ekasomo@corp.free.fr>
2024-05-31 11:56:02 +0200

    try to allow cors domains

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={
+    r"/*": {
+        "origins": ["localhost:5173", os.getenv('DB_PASS')]
+    }
+})
 
 from app import routes
