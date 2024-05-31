a78380cad1b54c5de68e17c86a1def537b892646
Emmanuel <ekasomo@corp.free.fr>
2024-05-31 12:07:30 +0200

    undo cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
 
 app = Flask(__name__)
-CORS(app, resources={
-    r"/*": {
-        "origins": ["localhost:5173", os.getenv('DB_PASS')]
-    }
-})
+CORS(app)
 
 from app import routes
